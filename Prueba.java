3fc114d98ccfd2966d26122a34040eb201a29c3d
Alejandro González <alejandro@falsogmail.com>
2018-10-03 12:35:25 +0200

    Ahora muestra mi ciudad de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba {
 	public static void main(String[] args) {
-		System.out.println("Alejandro Gonz\u00e1lez del Pe\u00f1o");
+		System.out.println("Alejandro Gonz\u00e1lez del Pe\u00f1o\nTrobajo del camino");
 	}
 }
